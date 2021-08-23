2d1cb7dcf99d1eb6844690f82caf189682298b31
kagikko <hirookagikko@gmail.com>
2021-08-24 05:45:25 +0900

    Update editriso_1.0.js

--- editRiso_pre/js/editriso_1.0.js
+++ editRiso_pre/js/editriso_1.0.js
@@ -3,4 +3,38 @@ const risoColors = [];
 for (const {name} of RISOCOLORS) {
   risoColors.push(name);
 }
-console.log(risoColors);
+
+let inkslot = {
+  A: 'BLUE',
+  B: 'RED',
+  C: 'YELLOW',
+  D: 'BLACK'
+}
+
+const formats = {
+  a5: {
+    name: 'A5',
+    width: 1748,
+    height: 2480,
+    printWidth: 148,
+    printHeight: 210
+  },
+  postcard: {
+    name: 'Post Card',
+    width: 1181,
+    height: 1748,
+    printWidth: 105,
+    printHeight: 148
+  },
+  cd: {
+    name: 'CD Jacket',
+    width: 1417,
+    height: 1417,
+    printWidth: 120,
+    printHeight: 120
+  }
+}
+
+function setup() {
+  // Toolbar
+}
